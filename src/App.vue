d21875e44cc9f0349ce6a9583953ab0050bab28b
DESKTOP-07IK3VB\Демид <demyd.vnukov@nure.ua>
2021-05-16 01:54:23 +0300

    Finished basic

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <main-wrapper/>
+  </div>
+</template>
+
+<script>
+import MainWrapper from './components/main-wrapper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainWrapper
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ vuex/store.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from'axios'
+Vue.use(Vuex);
+let store=new Vuex.Store({
+    //состояние
+    state:{
+        currencies: [],
+        cart:[]
+    },
+    //мутации
+    mutations:{
+
+        SET_CURRENCIES_TO_STATE:(state, currencies) =>{
+            state.currencies=currencies;
+        },
+
+    },
+
+    //действия
+    actions:{
+        GET_CURRENCY_FROM_API({commit}){
+            //вызываем аксиос запрос
+            return axios('https://api.kuna.io/v3/exchange-rates',{
+                method: "GET"
+            })
+                .then((currencies)=>  {
+                    console.log(currencies.data);
+                    commit ('SET_CURRENCIES_TO_STATE',currencies.data);
+                    return currencies;
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    return error;
+                })
+        },
+
+    },
+    //геттеры
+    getters:{
+        CURRENCIES(state){
+            return state.currencies;
+        }
+    }
+});
+export default store;
